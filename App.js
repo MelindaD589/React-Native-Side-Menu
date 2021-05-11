023d0e928fb5759ba2f5c973601d73807d83977b
Melinda Dothan <melinda.dothan@gmail.com>
2021-05-11 23:28:55 +0200

    Adding menu

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import SideMenu from "react-native-side-menu-updated";
 
-export default function App() {
+const ContentView = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+const Menu = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "column",
+      }}
+    >
+      <View style={styles.menuHeader}>
+        <Text style={styles.menuHeaderText}>Menu</Text>
+      </View>
+      <View style={{ flex: 9, backgroundColor: "green" }} />
+    </View>
+  );
+};
+
+const App = () => {
+  const menu = <Menu />;
+
+  return (
+    <SideMenu menu={menu}>
+      <ContentView />
+    </SideMenu>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  menuHeader: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  menuHeaderText: {
+    fontSize: 20,
+    color: "white",
   },
 });
+
+export default App;
